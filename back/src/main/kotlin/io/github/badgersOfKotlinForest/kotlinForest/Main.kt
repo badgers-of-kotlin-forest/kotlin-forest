8285dc692ed78e8030eaf0367ebfd4078b420560
Xetd71 <ischuev@edu.hse.ru>
2019-03-06 09:24:33 +0300

    Добавлена реализация итерации цикла (tick). Открытая проблема -- акторы при вызове метода move могут изменять других акторов и саму карту

--- back/src/main/kotlin/io/github/badgersOfKotlinForest/kotlinForest/Main.kt
+++ back/src/main/kotlin/io/github/badgersOfKotlinForest/kotlinForest/Main.kt
@@ -5,17 +5,21 @@ package io.github.badgersOfKotlinForest.kotlinForest
 Делается это через два цикла внутри системы, чтобы события происходили одновременно
 */
 class LifeCycle(val length: Int, val width: Int) {
+    val time: Long
+    val period: Long
     val height: Int
     val map: Array<Array<Array<MapItem>>>
 
     init {
+        time = 0
+        period = Long.MAX_VALUE
         height = 5
         map = Array(length) { Array(width) { Array(height) { MapItem() } } }
     }
 
     interface Actor {
-        // Приоритет действий акторов в одной клетке
-        val priority: Int
+        //        // Приоритет действий акторов в одной клетке
+//        val priority: Int
         // Периуд передвижения актора (обратно пропорционален его скорости)
         val movementPeriod: Int
 
@@ -35,8 +39,46 @@ class LifeCycle(val length: Int, val width: Int) {
         fun interact(otherActor: Actor): Actor?
     }
 
-    class MapItem(val actors: List<Actor>) {
-        constructor() : this(listOf())
+    class MapItem(val actors: MutableList<Actor>) {
+        constructor() : this(mutableListOf())
+    }
+
+    fun tick() {
+        val newMap = Array(length) { Array(width) { Array(height) { MapItem() } } }
+        for (x in 0..map.size) {
+            for (y in 0..map[x].size) {
+                for (z in 0..map[x][y].size) {
+                    for (actor in map[x][y][z].actors) {
+                        if (time % actor.movementPeriod == 0L) {
+                            val (x1, y1, z1) = actor.move(map, Triple(x, y, z))
+                            newMap[x1][y1][z1].actors.add(actor)
+                        }
+                    }
+                }
+            }
+        }
+
+        for (x in 0..newMap.size) {
+            for (y in 0..newMap[x].size) {
+                for (z in 0..newMap[x][y].size) {
+                    val actors = newMap[x][y][z].actors
+                    if (actors.size > 1) {
+                        val firstActor = actors.first()
+                        actors.removeAt(0)
+                        var curActor = firstActor
+                        do {
+                            for (actor in actors) {
+                                curActor.interact(actor)
+                            }
+                            actors.add(curActor)
+                            curActor = actors.first()
+                            actors.removeAt(0)
+                        } while (curActor != firstActor)
+                    }
+                    newMap[x][y][z] = MapItem(actors)
+                }
+            }
+        }
     }
 }
 
