e7234c7bce7b71f3d457a9169d8c6420d0cd5586
Xetd71 <ischuev@edu.hse.ru>
2019-03-13 20:54:57 +0300

    Добавлен абстрактный класс с описанием обычного животного Помечены дополнительные темы для обсуждения

--- back/src/main/kotlin/io/github/badgersOfKotlinForest/kotlinForest/actors/Actor.kt
+++ back/src/main/kotlin/io/github/badgersOfKotlinForest/kotlinForest/actors/Actor.kt
@@ -1,6 +1,8 @@
 package io.github.badgersOfKotlinForest.kotlinForest.actors
 
 interface Actor {
+    // TODO to discuss
+//    val forestMap: ForestMap
 
     fun interactWith(other: Actor)
 }
--- back/src/main/kotlin/io/github/badgersOfKotlinForest/kotlinForest/tree/Dwelling.kt
+++ back/src/main/kotlin/io/github/badgersOfKotlinForest/kotlinForest/tree/Dwelling.kt
@@ -4,4 +4,9 @@ import io.github.badgersOfKotlinForest.kotlinForest.animals.Animal
 
 interface Dwelling : TreeElement {
     val animalsInside: MutableList<Animal>
+
+    // TODO it's not correct to get into the dwelling by yourself
+//    fun getInto(animal: Animal)
+//
+//    fun getOut(animal: Animal)
 }
